9c384f004a1009ea2ba0f7476a5d56300c6151a5
Ryan Ferrier <ryan@ferriers.ca>
2025-10-02 20:02:27 -0600

    fixed and added toggle

--- app/src/main.c
+++ app/src/main.c
@@ -7,7 +7,7 @@
 
  #define LED0_NODE DT_ALIAS(led0)
 
- static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios)
+ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
  int main(void) {
     int ret;
@@ -22,7 +22,7 @@
     }
 
     while (1) {
-
+        gpio_pin_toggle_dt(&led0);
     }
 
     return 0;
