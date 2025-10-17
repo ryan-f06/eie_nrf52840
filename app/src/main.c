dca7bcc05d19e78fee304ff008381a74e1eaaeeb
Ryan Ferrier <ryan@ferriers.ca>
2025-10-16 20:14:14 -0600

    toggle led

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,26 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
+#include "BTN.h"
+#include "LED.h"
+
+#define SLEEP_MS 1
+
 int main(void) {
 
-  while(1) {
+  if (0 > BTN_init()) {
+    return 0;
+  } 
+  if (0 > LED_init()) {
+    return 0;
+  } 
 
+  while(1) {
+    if (BTN_check_clear_pressed(BTN0)) {
+      LED_toggle(LED0);
+      printk("Button 0 pressed!\n");
+    }
+    k_msleep(SLEEP_MS);
   }
 	return 0;
 }
