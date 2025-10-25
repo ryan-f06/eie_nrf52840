8152a453eb036962d9f512e67b9b107a807e87a1
Ryan Ferrier <ryan@ferriers.ca>
2025-10-24 20:37:04 -0600

    state machine combination lock

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -3,6 +3,7 @@
  */
 
 #include <zephyr/smf.h>
+#include <zephyr/sys/printk.h>
 
 #include "LED.h"
 #include "BTN.h"
@@ -11,26 +12,23 @@
 /**
  * Function Prototypes
  */
-static void state_0_entry(void* o);
-static enum smf_state_result state_0_run(void* o);
-static void state_1_entry(void* o);
-static enum smf_state_result state_1_run(void* o);
-static void state_2_entry(void* o);
-static enum smf_state_result state_2_run(void* o);
-static void state_3_entry(void* o);
-static enum smf_state_result state_3_run(void* o);
-static void state_4_entry(void* o);
-static enum smf_state_result state_4_run(void* o);
+static void start_up_entry(void* o);
+static enum smf_state_result start_up_run(void* o);
+static void set_combo_entry(void* o);
+static enum smf_state_result set_combo_run(void* o);
+static void inputting_entry(void* o);
+static enum smf_state_result inputting_run(void* o);
+static void waiting_entry(void* o);
+static enum smf_state_result waiting_run(void* o);
 
 /**
  * Typedefs
  */
 enum state_machine_states {
-    STATE_0,
-    STATE_1,
-    STATE_2,
-    STATE_3,
-    STATE_4
+    START_UP,
+    SET_COMBO,
+    INPUTTING,
+    WAITING
 };
 
 typedef struct {
@@ -38,134 +36,142 @@ typedef struct {
     struct smf_ctx ctx;
 
     uint16_t count;
+    uint16_t combo; //BUTTON 0 = 00, BUTTON 1 = 01, BUTTON 2 = 01
+    uint16_t correct_combo; // 0b01000100 (1-0-2-0)
 } state_object_t;
 
 /**
  * Local Variables
  */
+
 static const struct smf_state states[] = {
-    [STATE_0] = SMF_CREATE_STATE(state_0_entry, state_0_run, NULL, NULL, NULL),
-    [STATE_1] = SMF_CREATE_STATE(state_1_entry, state_1_run, NULL, NULL, NULL),
-    [STATE_2] = SMF_CREATE_STATE(state_2_entry, state_2_run, NULL, NULL, NULL),
-    [STATE_3] = SMF_CREATE_STATE(state_3_entry, state_3_run, NULL, NULL, NULL),
-    [STATE_4] = SMF_CREATE_STATE(state_4_entry, state_4_run, NULL, NULL, NULL),    
+    [START_UP] = SMF_CREATE_STATE(start_up_entry, start_up_run, NULL, NULL, NULL),
+    [SET_COMBO] = SMF_CREATE_STATE(set_combo_entry, set_combo_run, NULL, NULL, NULL),
+    [INPUTTING] = SMF_CREATE_STATE(inputting_entry, inputting_run, NULL, NULL, NULL),
+    [WAITING] = SMF_CREATE_STATE(waiting_entry, waiting_run, NULL, NULL, NULL)    
 };
 
 static state_object_t state_object;
 
 void state_machine_init() {
     state_object.count = 0;
-    smf_set_initial(SMF_CTX(&state_object), &states[STATE_0]);
+    state_object.correct_combo = 68; // 0b01000100 (1-0-2-0) default combo
+    smf_set_initial(SMF_CTX(&state_object), &states[START_UP]);
 }
 
 int state_machine_run() {
     return smf_run_state(SMF_CTX(&state_object));
 }
 
-static void state_0_entry(void* o) {
+static void start_up_entry(void* o) {
+    printk("start up\n");
     LED_set(LED0, LED_OFF);
     LED_set(LED1, LED_OFF);
     LED_set(LED2, LED_OFF);
-    LED_set(LED3, LED_OFF);
+    LED_set(LED3, LED_ON); 
 }
 
-static enum smf_state_result state_0_run(void* o) {
-    if (BTN_check_clear_pressed(BTN0)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_1]);
+static enum smf_state_result start_up_run(void* o) {
+    if (state_object.count > 3000){
+        state_object.count = 0;
+        smf_set_initial(SMF_CTX(&state_object), &states[INPUTTING]);     
+    } else {
+        state_object.count++;
+    }
+    if (BTN_check_clear_pressed(BTN3)) { //ENTER
+        smf_set_initial(SMF_CTX(&state_object), &states[SET_COMBO]); 
     }
 
     return SMF_EVENT_HANDLED;
 }
 
-static void state_1_entry(void* o) {
-    LED_set(LED0, LED_OFF);
-    LED_set(LED1, LED_OFF);
-    LED_set(LED2, LED_OFF);
-    LED_set(LED3, LED_OFF);
+static void set_combo_entry(void* o) {
+    printk("set combo\n");
+    LED_set(LED0, LED_ON);
+    LED_set(LED1, LED_ON);
+    LED_set(LED2, LED_ON);
+    LED_set(LED3, LED_ON);
+    state_object.correct_combo = 0; 
 }
 
-static enum smf_state_result state_1_run(void* o) {
-    if (state_object.count > 125) {
-        state_object.count = 0;
-        LED_toggle(LED0);
-    } else {
-        state_object.count++;
+static enum smf_state_result set_combo_run(void* o) {
+    if (BTN_check_clear_pressed(BTN3)) { //ENTER
+        state_object.correct_combo = state_object.correct_combo >> 2; //shift back into place
+        smf_set_initial(SMF_CTX(&state_object), &states[INPUTTING]); 
     }
-    if (BTN_check_clear_pressed(BTN3)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_0]);
+    if (BTN_check_clear_pressed(BTN0)) {
+        state_object.correct_combo += 0;
+        state_object.correct_combo = state_object.correct_combo << 2;
     }
     if (BTN_check_clear_pressed(BTN1)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_2]);
+        state_object.correct_combo += 1;
+        state_object.correct_combo = state_object.correct_combo << 2;
     }
     if (BTN_check_clear_pressed(BTN2)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_4]);
-    }  
+        state_object.correct_combo += 2;
+        state_object.correct_combo = state_object.correct_combo << 2;
+    }
 
     return SMF_EVENT_HANDLED;
 }
 
-static void state_2_entry(void* o) {
+static void inputting_entry(void* o) {
+    printk("inputting\n");
+    state_object.combo = 0;
     LED_set(LED0, LED_ON);
     LED_set(LED1, LED_OFF);
-    LED_set(LED2, LED_ON);
-    LED_set(LED3, LED_OFF);
-}
-
-static enum smf_state_result state_2_run(void* o) {
-    if (state_object.count > 1000) {
-        state_object.count = 0;
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_3]);
-    } else {
-        state_object.count++;    
-    }
-    if (BTN_check_clear_pressed(BTN3)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_0]);
-    }  
-
-    return SMF_EVENT_HANDLED;
-}
-
-static void state_3_entry(void* o) {
-    LED_set(LED0, LED_OFF);
-    LED_set(LED1, LED_ON);
-    LED_set(LED2, LED_OFF);
-    LED_set(LED3, LED_ON);
-}
-
-static enum smf_state_result state_3_run(void* o) {
-    if (state_object.count > 2000) {
-        state_object.count = 0;
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_2]);
-    } else {
-        state_object.count++;    
-    }
-    if (BTN_check_clear_pressed(BTN3)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_0]);
-    }  
-
-    return SMF_EVENT_HANDLED;
-}
-
-static void state_4_entry(void* o) {
-    LED_set(LED0, LED_OFF);
-    LED_set(LED1, LED_OFF);
     LED_set(LED2, LED_OFF);
     LED_set(LED3, LED_OFF);
 }
 
-static enum smf_state_result state_4_run(void* o) {
-    if (state_object.count > 62) {
+static enum smf_state_result inputting_run(void* o) {
+    if (BTN_check_clear_pressed(BTN3)) { //ENTER
+        state_object.combo = state_object.combo >> 2; //shift back into place
+        smf_set_state(SMF_CTX(&state_object), &states[WAITING]);
+    }
+    if (BTN_check_clear_pressed(BTN0)) {
+        state_object.combo += 0;
+        state_object.combo = state_object.combo << 2;
+    }
+    if (BTN_check_clear_pressed(BTN1)) {
+        state_object.combo += 1;
+        state_object.combo = state_object.combo << 2;
+    }
+    if (BTN_check_clear_pressed(BTN2)) {
+        state_object.combo += 2;
+        state_object.combo = state_object.combo << 2;
+    }
+
+    return SMF_EVENT_HANDLED;
+}
+
+static void waiting_entry(void* o) {
+    printk("enterd combo: %d\n", state_object.combo);
+    printk("correct combo: %d\n", state_object.correct_combo);
+    if (state_object.combo == state_object.correct_combo) {         
+        LED_set(LED0, LED_ON);
+        LED_set(LED1, LED_ON);
+        LED_set(LED2, LED_ON);
+        LED_set(LED3, LED_ON);
+    } else {
+        LED_set(LED0, LED_OFF);
+        LED_set(LED1, LED_OFF);
+        LED_set(LED2, LED_OFF);
+        LED_set(LED3, LED_OFF);
+    }
+}
+
+static enum smf_state_result waiting_run(void* o) {
+    if (state_object.count > 1000){
         state_object.count = 0;
-        LED_toggle(LED0);
-        LED_toggle(LED1);
-        LED_toggle(LED2);
-        LED_toggle(LED3);
+        LED_toggle(LED3); 
     } else {
         state_object.count++;
     }
-    if (BTN_check_clear_pressed(BTN3)) {
-        smf_set_state(SMF_CTX(&state_object), &states[STATE_0]);
-    }  
+    if (BTN_check_clear_pressed(BTN3)) { //ENTER
+        state_object.count = 0;
+        smf_set_state(SMF_CTX(&state_object), &states[INPUTTING]); //LOCK
+    }
 
     return SMF_EVENT_HANDLED;
 }
