a2509d60d8bc7460c83a7c9a3b6f11adeb35b15e
Ryan Ferrier <ryan@ferriers.ca>
2025-10-30 19:10:57 -0600

    duty cycle LED

--- app/src/main.c
+++ app/src/main.c
@@ -21,7 +21,16 @@ int main(void) {
     return 0;
   }
 
+  uint8_t current_duty_cycle = 0;
+
+  LED_pwm(LED0, current_duty_cycle);
+
   while(1) {
+    if (BTN_check_clear_pressed(BTN0)) {
+      current_duty_cycle = (current_duty_cycle >= 100) ? 0 : (current_duty_cycle + 10);
+      printk("Setting LED0 to %d%% brightness.\n", current_duty_cycle);
+      LED_pwm(LED0, current_duty_cycle);
+    }
     k_msleep(SLEEP_MS);
   }
 	return 0;
