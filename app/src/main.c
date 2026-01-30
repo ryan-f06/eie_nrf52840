d579c1d1845f65802e5e86880d65eb7d2ff83720
Ryan Ferrier <ryan@ferriers.ca>
2026-01-29 20:06:25 -0700

    working screen w btns and pic

--- app/src/main.c
+++ app/src/main.c
@@ -6,13 +6,37 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/display.h>
+
+#include <lvgl.h>
 
 #include "BTN.h"
 #include "LED.h"
+#include "lv_data_obj.h"
 
 #define SLEEP_MS 1
 
+static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+static lv_obj_t *screen = NULL;
+
+extern const lv_image_dsc_t circuit;
+
+void lv_button_callback(lv_event_t *event) {
+  lv_obj_t *data_obj = (lv_obj_t *)lv_event_get_user_data(event);
+  led_id led = *(led_id *)lv_data_obj_get_data_ptr(data_obj);
+
+  LED_toggle(led);
+}
+
 int main(void) {
+  if (!device_is_ready(display_dev)) {
+    return 0;
+  }
+  screen = lv_screen_active();
+  if (screen == NULL) {
+    return 0;
+  }
+
   if (0 > BTN_init()) {
     return 0;
   }
@@ -20,7 +44,29 @@ int main(void) {
     return 0;
   }
 
+  lv_obj_t *image = lv_image_create(screen);
+  lv_image_set_src(image, &circuit);
+  lv_obj_align(image, LV_ALIGN_CENTER, 0, 0);
+
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    lv_obj_t *ui_btn = lv_button_create(screen);
+    // place the buttons in a 2x2 grid in the center of the screen
+    // matching the orientations of the LEDs on the board
+    lv_obj_align(ui_btn, LV_ALIGN_CENTER, 50 * (i % 2 ? 1 : -1), 20 * (i < 2 ? -1 : 1));
+    lv_obj_t *button_label = lv_label_create(ui_btn);
+    char label_text[10];
+    snprintf(label_text, 10, "LED %d", i);
+    lv_label_set_text(button_label, label_text);
+    lv_obj_align(button_label, LV_ALIGN_CENTER, 0, 0);
+
+    led_id led = (led_id)i;
+    lv_obj_t *data_obj = lv_data_obj_create_alloc_assign(ui_btn, &led, sizeof(led_id));
+    lv_obj_add_event_cb(ui_btn, lv_button_callback, LV_EVENT_CLICKED, data_obj);
+  }
+
+  display_blanking_off(display_dev);
   while (1) {
+    lv_timer_handler();
     k_msleep(SLEEP_MS);
   }
   return 0;
